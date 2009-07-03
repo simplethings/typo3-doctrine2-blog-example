1f5de2085ee5b6e8774d878b92ff2bf217e127e3
Jochen Rau <j.rau@web.de>
2009-07-03 10:35:02 +0000

    BlogExample: * moved all files to trunk

---
+++ Module/BlogModule/conf.php
@@ -0,0 +1,12 @@
+<?php
+
+    // DO NOT REMOVE OR CHANGE THESE 2 LINES:
+$MCONF['name'] = 'tools_txblogexampleM1';
+//$MCONF['script'] = '_DISPATCH';
+$MCONF['script'] = '_DISPATCH';
+    
+$MCONF['access'] = 'user,group';
+
+$MLANG['default']['tabs_images']['tab'] = '../../ext_icon.gif';
+$MLANG['default']['ll_ref'] = 'LLL:EXT:blogexample/Resources/Private/Language/locallang_mod.xml';
+?> 
---
+++ Module/BlogModule/index.php
@@ -0,0 +1,49 @@
+<?php
+/***************************************************************
+*  Copyright notice
+*
+*  (c) 2009 Fabien Udriot <fabien.udriot@ecodev.ch>
+*  All rights reserved
+*
+*  This script is part of the TYPO3 project. The TYPO3 project is
+*  free software; you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation; either version 2 of the License, or
+*  (at your option) any later version.
+*
+*  The GNU General Public License can be found at
+*  http://www.gnu.org/copyleft/gpl.html.
+*
+*  This script is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*
+*  This copyright notice MUST APPEAR in all copies of the script!
+***************************************************************/
+
+
+/**
+ * Default index for the BE module
+ *
+ * @author	Steffen Kamper <info@sk-typo3.de>
+ * @package	TYPO3
+ * @subpackage	tx_addresses
+ * @copyright Copyright belongs to the respective authors
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License, version 2
+ */
+
+// temporary index.php to get it started now
+include_once(t3lib_extMgm::extPath('blogexample', 'Classes/Controller/ModuleController.php'));
+
+$LANG->includeLLFile('EXT:blogexample/Resources/Private/Language/locallang.xml');
+$BE_USER->modAccess($MCONF,1);    // This checks permissions and exits if the users has no permission for entry.
+
+$SOBE = t3lib_div::makeInstance('Tx_Blog_Controller_ModuleController');
+
+// Include files?
+foreach($SOBE->include_once as $INC_FILE)    include_once($INC_FILE);
+$SOBE->main();
+
+   
+?>
