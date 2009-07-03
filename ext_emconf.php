bb7d0337fe7ba0f09bbbce78a0d405b79639217e
Jochen Rau <j.rau@web.de>
2009-07-03 10:34:50 +0000

    BlogExample: * moved all files to trunk

---
+++ ext_emconf.php
@@ -0,0 +1,50 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext: "blog_example"
+#
+# Auto generated 
+#
+# Manual updates:
+# Only the data in the array - anything else is removed by next write.
+# "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'A Blog Example for the Extbase Framework',
+	'description' => 'An example extension demonstrating the features of the Extbase Framework. It is the back-ported Blog Example package of FLOW3. Have fun playing with it!',
+	'category' => 'plugin',
+	'author' => 'Jochen Rau',
+	'author_company' => 'typoplanet',
+	'author_email' => 'jochen.rau@typoplanet.de',
+	'shy' => '',
+	'dependencies' => 'extbase,fluid',
+	'conflicts' => '',
+	'priority' => '',
+	'module' => '',
+	'state' => 'alpha',
+	'internal' => '',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 1,
+	'lockType' => '',
+	'version' => '0.9.0',
+	'constraints' => array(
+		'depends' => array(
+			'php' => '5.2.0-0.0.0',
+			'typo3' => '4.3.dev-4.3.99',
+			'extbase' => '0.0.0-0.0.0',
+			'fluid' => '0.0.0-0.0.0',
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => '',
+	'suggests' => array(
+	),
+);
+
+?>
