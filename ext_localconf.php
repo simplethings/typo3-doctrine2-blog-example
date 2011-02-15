8bd3fca3d4c4753fad1a8d3b68b8e90367b05105
Bastian Waidelich <bastian@typo3.org>
2011-02-15 19:00:22 +0100

    [!!!][+TASK] BlogExample: Adjust BlogExample to Extbase 1.3
    This commit incorporates following changes:
    - Updated version from 1.2 to 1.3
    - Set dependency to Extbase/Fluid v1.3 and TYPO3 v4.5
    - General cleanup according to CGL, some minor tweaks
    - Renamed templates & layouts to be UpperCamelCase
    - Localization of all aspects (templates, flashmessages, validation errors)
    - Improved plaintext rendering
    - Administrative actions are only visible to logged in FE users
    - Use of pagination widget for blog and post listings
    - Added exemplary CSS styles and made templates (X)HTML valid
    
    Change-Id: I3e6bef67abb8b7c5b1e5500cfe8de3771a66f89b

--- ext_localconf.php
+++ ext_localconf.php
@@ -11,16 +11,17 @@ $GLOBALS['TYPO3_CONF_VARS']['EXTCONF'][$_EXTKEY] = unserialize($_EXTCONF);
 if ($GLOBALS['TYPO3_CONF_VARS']['EXTCONF'][$_EXTKEY]['registerSinglePlugin']) {
 	// fully fletged blog
 	Tx_Extbase_Utility_Extension::configurePlugin(
-		$_EXTKEY,																		// The extension name (in UpperCamelCase) or the extension key (in lower_underscore)
-		'Pi1',																			// A unique name of the plugin in UpperCamelCase
-		array (																			// An array holding the controller-action-combinations that are accessible
-			'Blog' => 'index,new,create,delete,deleteAll,edit,update,populate',	        // The first controller and its first action will be the default
+		$_EXTKEY,																	// The extension name (in UpperCamelCase) or the extension key (in lower_underscore)
+		'Pi1',																		// A unique name of the plugin in UpperCamelCase
+		array (																		// An array holding the controller-action-combinations that are accessible
+			'Blog' => 'index,new,create,delete,deleteAll,edit,update,populate',		// The first controller and its first action will be the default
 			'Post' => 'index,show,new,create,delete,edit,update',
-			'Comment' => 'create'
-		), array(																		// An array of non-cachable controller-action-combinations (they must already be enabled)
+			'Comment' => 'create,delete'
+		),
+		array(																		// An array of non-cachable controller-action-combinations (they must already be enabled)
 			'Blog' => 'create,delete,deleteAll,update,populate',
 			'Post' => 'create,delete,update',
-			'Comment' => 'create'
+			'Comment' => 'create,delete'
 		)
 	);
 } else {
@@ -51,10 +52,13 @@ if ($GLOBALS['TYPO3_CONF_VARS']['EXTCONF'][$_EXTKEY]['registerSinglePlugin']) {
 		'BlogAdmin',
 		array(
 			'Blog' => 'new,create,delete,deleteAll,edit,update,populate',
-			'Post' => 'new,create,delete,edit,update'),
+			'Post' => 'new,create,delete,edit,update',
+			'Comment' => 'delete',
+		),
 		array(
 			'Blog' => 'create,delete,deleteAll,update,populate',
-			'Post' => 'create,delete,update'
+			'Post' => 'create,delete,update',
+			'Comment' => 'delete',
 		)
 	);
 }
--- ext_tables.php
+++ ext_tables.php
@@ -1,6 +1,8 @@
 <?php
 if (!defined ('TYPO3_MODE')) die ('Access denied.');
 
+$extensionName = t3lib_div::underscoredToUpperCamelCase($_EXTKEY);
+
 /**
  * Registers a Plugin to be listed in the Backend. You also have to configure the Dispatcher in ext_localconf.php.
  */
@@ -8,34 +10,43 @@ if ($GLOBALS['TYPO3_CONF_VARS']['EXTCONF'][$_EXTKEY]['registerSinglePlugin']) {
 	Tx_Extbase_Utility_Extension::registerPlugin(
 		$_EXTKEY,// The extension name (in UpperCamelCase) or the extension key (in lower_underscore)
 		'Pi1',				// A unique name of the plugin in UpperCamelCase
-		'A Blog Example',	// A title shown in the backend dropdown field
-		t3lib_extMgm::extRelPath($_EXTKEY) . 'Resources/Public/Icons/icon_tx_blogexample_domain_model_blog.gif'
+		'A Blog Example'	// A title shown in the backend dropdown field
 	);
+
+	$pluginSignature = strtolower($extensionName) . '_pi1';
+	$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist'][$pluginSignature] = 'select_key';
+	$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature] = 'pi_flexform,recursive';
+	t3lib_extMgm::addPiFlexFormValue($pluginSignature, 'FILE:EXT:' . $_EXTKEY . '/Configuration/FlexForms/flexform_list.xml');
 } else {
+	t3lib_div::loadTCA('tt_content');
+	//$TCA['tt_content']['columns']['list_type']['config']['iconsInOptionTags'] = 0;
+	$TCA['tt_content']['columns']['list_type']['config']['items'][] = array('Blog Example', '--div--', t3lib_extMgm::extRelPath($_EXTKEY) . 'ext_icon.gif');
 	Tx_Extbase_Utility_Extension::registerPlugin(
 		$_EXTKEY,
 		'BlogList',
-		'List of Blogs (BlogExample)',
-		t3lib_extMgm::extRelPath($_EXTKEY) . 'Resources/Public/Icons/icon_tx_blogexample_domain_model_blog.gif'
+		'List of Blogs (BlogExample)'
 	);
 	Tx_Extbase_Utility_Extension::registerPlugin(
 		$_EXTKEY,
 		'PostList',
-		'List of Posts (BlogExample)',
-		t3lib_extMgm::extRelPath($_EXTKEY) . 'Resources/Public/Icons/icon_tx_blogexample_domain_model_post.gif'
+		'List of Posts (BlogExample)'
 	);
 	Tx_Extbase_Utility_Extension::registerPlugin(
 		$_EXTKEY,
 		'PostSingle',
-		'Single Post (BlogExample)',
-		t3lib_extMgm::extRelPath($_EXTKEY) . 'Resources/Public/Icons/icon_tx_blogexample_domain_model_post.gif'
+		'Single Post (BlogExample)'
 	);
 	Tx_Extbase_Utility_Extension::registerPlugin(
 		$_EXTKEY,
 		'BlogAdmin',
-		'Admin Plugin (BlogExample)',
-		t3lib_extMgm::extRelPath($_EXTKEY) . 'Resources/Public/Icons/icon_tx_blogexample_domain_model_blog.gif'
+		'Admin Plugin (BlogExample)'
 	);
+	$TCA['tt_content']['columns']['list_type']['config']['items'][] = array('', '--div--');
+
+	$pluginSignature = strtolower($extensionName) . '_postlist';
+	$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist'][$pluginSignature] = 'select_key';
+	$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature] = 'pi_flexform,recursive';
+	t3lib_extMgm::addPiFlexFormValue($pluginSignature, 'FILE:EXT:' . $_EXTKEY . '/Configuration/FlexForms/flexform_list.xml');
 }
 
 if (TYPO3_MODE === 'BE')	{
@@ -66,7 +77,8 @@ if (TYPO3_MODE === 'BE')	{
  */
 t3lib_extMgm::addLLrefForTCAdescr('_MOD_web_BlogExampleTxBlogexampleM1', 'EXT:' . $_EXTKEY . '/Resources/Private/Language/locallang_csh.xml');
 
-t3lib_extMgm::addStaticFile($_EXTKEY, 'Configuration/TypoScript', 'Blog Example');
+t3lib_extMgm::addStaticFile($_EXTKEY, 'Configuration/TypoScript', 'BlogExample setup');
+t3lib_extMgm::addStaticFile($_EXTKEY, 'Configuration/TypoScript/DefaultStyles', 'BlogExample CSS Styles (optional)');
 
 t3lib_extMgm::allowTableOnStandardPages('tx_blogexample_domain_model_blog');
 $TCA['tx_blogexample_domain_model_blog'] = array (
@@ -175,12 +187,4 @@ if (is_array($TCA['fe_users']['columns']['tx_extbase_type'])) {
 	$TCA['fe_users']['types']['Tx_BlogExample_Domain_Model_Administrator'] = $TCA['fe_users']['types']['0'];
 	array_push($TCA['fe_users']['columns']['tx_extbase_type']['config']['items'], array('LLL:EXT:blog_example/Resources/Private/Language/locallang_db.xml:fe_users.tx_extbase_type.Tx_BlogExample_Domain_Model_Administrator', 'Tx_BlogExample_Domain_Model_Administrator'));
 }
-
-$extensionName = t3lib_div::underscoredToUpperCamelCase($_EXTKEY);
-$pluginSignature = strtolower($extensionName) . '_pi1';
-
-$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist'][$pluginSignature] = 'layout,select_key,recursive';
-$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature] = 'pi_flexform';
-t3lib_extMgm::addPiFlexFormValue($pluginSignature, 'FILE:EXT:' . $_EXTKEY . '/Configuration/FlexForms/flexform_list.xml');
-
 ?>
